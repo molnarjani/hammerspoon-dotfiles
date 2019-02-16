2ad6a0f0c2d6444b6c48fd12e6d1c87f07bfddb1
Janos Molnar <janosmolnar@janosmolnar.local>
2019-02-16 19:44:46 +0100

    Replace some of my own code with spoons that do pretty much the same

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
+
+hyper = {"cmd", "alt", "ctrl"}
+hyperShift = {"cmd", "alt", "ctrl", "shift"}
+
+Install:andUse('WindowScreenLeftAndRight')
+Install:andUse('WindowHalfsAndThirds')
+
+spoon.WindowScreenLeftAndRight:bindHotkeys({
+    screen_left = { hyper, "N" },
+    screen_right= { hyper, "P" },
+})
+
+spoon.WindowHalfsAndThirds:bindHotkeys({
+    left_half   = { hyper, "Left" },
+    right_half  = { hyper, "Right" },
+})
+
 function reloadConfig(files)
     local doReload = false
     for _,file in pairs(files) do
--- window_bindings.lua
+++ window_bindings.lua
@@ -1,22 +1,3 @@
-function shift (direction)
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	f.w = max.w / 2
-
-	if direction == "Left" then
-		f.x = max.x
-		win:setFrame(f)
-	end
-
-	if direction == "Right" then
-		f.x = max.w - (max.w / 2)
-		win:setFrame(f)
-	end
-end
-
 function maximize ()
 	local win = hs.window.focusedWindow()
 	local f = win:frame()
@@ -27,23 +8,14 @@ function maximize ()
 	win:setFrame(f)
 end
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
-    shift("Left")
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
-    shift("Right")
-end)
-
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
 	hs.window.switcher.nextWindow()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
-	hs.window.switcher.nextWindow()
+	hs.window.switcher.previousWindow()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
     maximize()
 end)
-
